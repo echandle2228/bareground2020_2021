27e65c1f3c4b3ee3a7e3d3ed6da202396ad6bb13
echandle2228 <echandle@cord.edu>
2021-02-28 21:41:47 -0600

    Figured out some FFD stats for the paper and played around with a different version of the boxplot

--- Program/FFD_boxplot.R
+++ Program/FFD_boxplot.R
@@ -2,7 +2,6 @@
 
 library(ggplot2)
 
-
 ffd4 <- read.csv(file="Data/FFD_boxplot.csv",header=T, stringsAsFactors = F)
 # make boxplots
 
@@ -17,4 +16,23 @@ z <- ggplot(ffd4, aes(ID, FFD))+
         axis.text.x=element_text(size = 12),
         axis.title.x=element_text(size = 14))
 plot(z)
-  
+
+
+ffd4_nona <- na.omit(ffd4)
+aggregate(ffd4_nona$FFD,         # Median by group
+          list(ffd4_nona$Name),
+          median)
+
+library(forcats)
+z_names <- ggplot(ffd4, aes(x=reorder(Name, FFD, na.rm = TRUE), y = FFD))+
+  geom_boxplot()+
+  theme_classic() +
+  ylab("FFD")+ 
+  xlab("Species") +
+  theme(text = element_text(family = "serif"),
+        axis.text.y=element_text(size=12),
+        axis.title.y=element_text(size=14),
+        axis.text.x=element_text(size = 12, angle = 45, vjust = 1, hjust = 1, face = "italic"),
+        axis.title.x=element_text(size = 14),
+        plot.margin = margin(0.5,0,0,1.2, "cm"))
+plot(z_names)
